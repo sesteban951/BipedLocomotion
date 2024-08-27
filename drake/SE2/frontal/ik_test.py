cba4a1e536735ec5fc31c91a859a61d23ffe1e33
sesteban951 <sesteban@caltech.edu>
2024-08-26 20:49:36 -0700

    played around with costs vs constraints in the IK problem. Costs in general are solved faster than constraints

--- drake/SE2/frontal/ik_test.py
+++ drake/SE2/frontal/ik_test.py
@@ -19,14 +19,6 @@ q0 = np.array([0, 1.0,
                0, 0, 0, 0, 
                0, 0, 0, 0])  # initial condition
 
-# no arms (parallel legs, bent knees)
-q_nominal = np.array([
-    0.0000, 0.9300,           # base position
-    0.0000,                   # base orientation
-    0, -0.5515, 1.0239,-0.4725,  # left leg
-    0, -0.5515, 1.0239,-0.4725,  # left leg
-])
-
 # set plant configruation 
 plant.SetPositions(plant_context, q0)
 
@@ -42,107 +34,121 @@ ik = InverseKinematics(plant, with_joint_limits=True)
 
 # add a quadratic error cost to the solver
 # cost = 0.5 * (q_hat - q_nominal)^T * Q * (q_hat - q_nominal)
-ik.prog().AddQuadraticErrorCost(Q=np.eye(plant.num_positions()), 
-                                x_desired=q_nominal, vars=ik.q())
+# no arms (parallel legs, bent knees)
+q_nominal = np.array([
+    0.0000, 0.9300,           # base position
+    0.0000,                   # base orientation
+    0, -0.5515, 1.0239,-0.4725,  # left leg
+    0, -0.5515, 1.0239,-0.4725,  # left leg
+])
+# Q = np.diag([0, 0,
+#              1, 
+#              1, 1, 1, 1, 
+#              1, 1, 1, 1]) * 1
+# ik.prog().AddQuadraticErrorCost(Q=np.eye(plant.num_positions()), 
+#                                 x_desired=q_nominal, vars=ik.q())
 
-# inverse kinematics solver settings
-epsilon_feet = 0.003     # foot position tolerance     [m]
-epsilon_base = 0.003     # torso position tolerance    [m]
-foot_epsilon_orient = 1.0   # foot orientation tolerance  [deg]
-base_epsilon_orient = 1.0   # torso orientation tolerance [deg]
-tol_base = np.array([[np.inf], [epsilon_base], [epsilon_base]])  # y-z only
-tol_feet = np.array([[epsilon_feet], [epsilon_feet], [epsilon_feet]])  # y-z only
+# # inverse kinematics solver settings
+# epsilon_feet = 0.003     # foot position tolerance     [m]
+# epsilon_base = 0.003     # torso position tolerance    [m]
+# foot_epsilon_orient = 1.0   # foot orientation tolerance  [deg]
+# base_epsilon_orient = 1.0   # torso orientation tolerance [deg]
+# tol_base = np.array([[np.inf], [epsilon_base], [epsilon_base]])  # y-z only
+# tol_feet = np.array([[epsilon_feet], [epsilon_feet], [epsilon_feet]])  # y-z only
 
-# Add com position constraint
-p_com_cons = ik.AddPositionConstraint(static_com_frame, [0, 0, 0], 
-                                      plant.world_frame(), 
-                                      [0,0,0], [0,0,0])
+# t1 = time.time()
+# # Add com position constraint
+# p_com_cons = ik.AddPositionConstraint(static_com_frame, [0, 0, 0], 
+#                                       plant.world_frame(), 
+#                                       [0,0,0], [0,0,0])
 
-# Add com orientation constraint
-rpy_base = RollPitchYaw([0, 0, 0])
-R_base = RotationMatrix(rpy_base)
-r_com_cons = ik.AddOrientationConstraint(static_com_frame, RotationMatrix(),
-                                         plant.world_frame(), R_base,
-                                         base_epsilon_orient * (np.pi/180))
+# # Add com orientation constraint
+# rpy_base = RollPitchYaw([0, 0, 0])
+# R_base = RotationMatrix(rpy_base)
+# r_com_cons = ik.AddOrientationConstraint(static_com_frame, RotationMatrix(),
+#                                          plant.world_frame(), R_base,
+#                                          base_epsilon_orient * (np.pi/180))
 
-# Add foot position constraints
-p_left_cons = ik.AddPositionConstraint(left_foot_frame, [0, 0, 0],
-                                        plant.world_frame(), 
-                                        [0,0,0], [0,0,0])
-p_right_cons = ik.AddPositionConstraint(right_foot_frame, [0, 0, 0],
-                                        plant.world_frame(), 
-                                        [0,0,0], [0,0,0])
+# # Add foot position constraints
+# p_left_cons = ik.AddPositionConstraint(left_foot_frame, [0, 0, 0],
+#                                         plant.world_frame(), 
+#                                         [0,0,0], [0,0,0])
+# p_right_cons = ik.AddPositionConstraint(right_foot_frame, [0, 0, 0],
+#                                         plant.world_frame(), 
+#                                         [0,0,0], [0,0,0])
 
-# Add foot orientation constraints
-r_right_cons = ik.AddAngleBetweenVectorsConstraint(right_foot_frame, [0, 0, 1],
-                                                   plant.world_frame(), [0, 0, 1],
-                                                   0, foot_epsilon_orient * (np.pi/180))
-r_left_cons = ik.AddAngleBetweenVectorsConstraint(left_foot_frame, [0, 0, 1],
-                                                  plant.world_frame(), [0, 0, 1],
-                                                  0, foot_epsilon_orient * (np.pi/180))
+# # Add foot orientation constraints
+# r_right_cons = ik.AddAngleBetweenVectorsConstraint(right_foot_frame, [0, 0, 1],
+#                                                    plant.world_frame(), [0, 0, 1],
+#                                                    0, foot_epsilon_orient * (np.pi/180))
+# r_left_cons = ik.AddAngleBetweenVectorsConstraint(left_foot_frame, [0, 0, 1],
+#                                                   plant.world_frame(), [0, 0, 1],
+#                                                   0, foot_epsilon_orient * (np.pi/180))
+# t2 = time.time()
+# T1 = t2-t1
 
 #############################################################################################
 
-# solve the IK problem 
-p_static_com = plant.CalcPointsPositions(plant_context, 
-                                           static_com_frame, 
-                                           [0, 0, 0], 
-                                           plant.world_frame())
-R_static_com = plant.CalcRelativeTransform(plant_context,
-                                             plant.world_frame(),
-                                             static_com_frame).rotation()
-p_left = plant.CalcPointsPositions(plant_context, 
-                                          left_foot_frame, 
-                                          [0, 0, 0], 
-                                          plant.world_frame())
-R_left = plant.CalcRelativeTransform(plant_context,
-                                        plant.world_frame(),
-                                        left_foot_frame).rotation()
-p_right = plant.CalcPointsPositions(plant_context, 
-                                           right_foot_frame, 
-                                           [0, 0, 0], 
-                                           plant.world_frame())
-R_right = plant.CalcRelativeTransform(plant_context,
-                                             plant.world_frame(),
-                                             right_foot_frame).rotation()
-print("p_static_com: ", p_static_com)
-# print("R_static_com: ", R_static_com)
-print("p_left: ", p_left)
-# print("R_left: ", R_left)
-print("p_right: ", p_right)
-# print("R_right: ", R_right)
-print("*"*50)
+# set the desired target positions
+p_com_target  = np.array([0.0, .0, 0.75]).reshape(3,1)
+p_left_target = np.array([0, 0.1, 0.20]).reshape(3,1)
+p_right_target = np.array([0, -0.1, 0.20]).reshape(3,1)
 
-# p_com_target = p_static_com_W
-p_com_target  = np.array([0.0, .0, 0.68]).reshape(3,1)
-p_left_target = np.array([0, 0.1, 0.15]).reshape(3,1)
-p_right_target = np.array([0, -0.1, 0.15]).reshape(3,1)
+# # update the constraints
+# t1 = time.time()
+# p_com_cons.evaluator().UpdateLowerBound(p_com_target - tol_base)
+# p_com_cons.evaluator().UpdateUpperBound(p_com_target + tol_base)
+# p_left_cons.evaluator().UpdateLowerBound(p_left_target - tol_feet)
+# p_left_cons.evaluator().UpdateUpperBound(p_left_target + tol_feet)
+# p_right_cons.evaluator().UpdateLowerBound(p_right_target - tol_feet)
+# p_right_cons.evaluator().UpdateUpperBound(p_right_target + tol_feet)
+# t2 = time.time()
+# T2 = t2-t1
+# print("Time to create and update hard constraints: ", T1, T2)
 
-p_com_cons.evaluator().UpdateLowerBound(p_com_target - tol_base)
-p_com_cons.evaluator().UpdateUpperBound(p_com_target + tol_base)
-p_left_cons.evaluator().UpdateLowerBound(p_left_target - tol_feet)
-p_left_cons.evaluator().UpdateUpperBound(p_left_target + tol_feet)
-p_right_cons.evaluator().UpdateLowerBound(p_right_target - tol_feet)
-p_right_cons.evaluator().UpdateUpperBound(p_right_target + tol_feet)
+# add the costs
+t0 = time.time()
+C = np.diag([1, 1, 1])*10
+p_com_cons = ik.AddPositionCost(static_com_frame, [0, 0, 0],
+                                plant.world_frame(), p_com_target,
+                                C)
 
-print("com IK")
-print(p_com_cons.evaluator().lower_bound())
-print(p_com_target.T)
-print(p_com_cons.evaluator().upper_bound())
+C = 10.0
+r_com_cons = ik.AddOrientationCost(static_com_frame, RotationMatrix(),
+                                   plant.world_frame(), RotationMatrix(),
+                                   C)
 
-print("Left")
-print(p_left_cons.evaluator().lower_bound())
-print(p_left_target)
-print(p_left_cons.evaluator().upper_bound())
+C = np.diag([1, 1, 1])*10
+p_left_cons = ik.AddPositionCost(left_foot_frame, [0, 0, 0],
+                                plant.world_frame(), p_left_target,
+                                C)
+p_right_cons = ik.AddPositionCost(right_foot_frame, [0, 0, 0],
+                                plant.world_frame(), p_right_target,
+                                C)
 
-print("Right")
-print(p_right_cons.evaluator().lower_bound())
-print(p_right_target.T)
-print(p_right_cons.evaluator().upper_bound())
+C = 10.0
+r_right_cons = ik.AddAngleBetweenVectorsCost(right_foot_frame, [0, 0, 1],
+                                            plant.world_frame(), [0, 0, 1],
+                                            C)
+r_left_cons = ik.AddAngleBetweenVectorsCost(left_foot_frame, [0, 0, 1],
+                                            plant.world_frame(), [0, 0, 1],
+                                            C)
+t1 = time.time()
+print("Add costs time: ", t1-t0)
 
 # solve the IK problem
-ik.prog().SetInitialGuess(ik.q(), q0)
-res = SnoptSolver().Solve(ik.prog())
+# refer to https://web.stanford.edu/group/SOL/guides/sndoc7.pdf for solver options
+solver = SnoptSolver()
+solver_options = SolverOptions()
+solver_options.SetOption(solver.solver_id(), "Major optimality tolerance", 1e-4)  # Example tolerance
+solver_options.SetOption(solver.solver_id(), "Linesearch tolerance", 0.9)
+
+t0 = time.time()
+ik.prog().SetInitialGuess(ik.q(), q_nominal)
+res = solver.Solve(ik.prog(), None, solver_options)
+# res = SnoptSolver().Solve(ik.prog())
+t1 = time.time()
+print("IK solve time: ", t1-t0)
 
 if res.is_success():
 
