6f6d91375c22b057a5c3ebb0a1bb38d6c654af9e
sesteban951 <sesteban@caltech.edu>
2025-06-08 20:32:46 -0700

    added actuators to URDF and reduced the contact geometries to the 4 points per foot

--- sim/utils/visualize_model.py
+++ sim/utils/visualize_model.py
@@ -11,7 +11,7 @@ from pydrake.all import ModelVisualizer, MultibodyPlant, Parser, StartMeshcat
 from pydrake.all import *
 
 # load model
-urdf_path = "../../models/achilles_drake.urdf"
+urdf_path = "../../models/g1_12dof_obj.urdf"
 plant = MultibodyPlant(0)
 Parser(plant).AddModels(urdf_path)
 plant.Finalize()
@@ -19,7 +19,7 @@ plant.Finalize()
 ########################################################################################
 
 # model instance index
-model_instance_idx = plant.GetModelInstanceByName("achilles")
+model_instance_idx = plant.GetModelInstanceByName("g1_12dof")
 
 # BODIES
 print("-"*50)
