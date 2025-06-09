71a094a8f6db38e66b860425a696eb0648a931ed
sesteban951 <sesteban@caltech.edu>
2025-06-08 20:33:12 -0700

    super preliminary G1 walking in a straight line

--- sim/src/mpc_g1.py
+++ sim/src/mpc_g1.py
@@ -54,6 +54,69 @@ config_path = "../config/config_g1.yaml"
 with open(config_path, 'r') as file:
     config = yaml.safe_load(file)
 
+########################################################################################
+
+class IDX():
+    """
+    Class to hold the indices of the model instance.
+    """
+    def __init__(self):
+        
+        # generalized positions
+        self.QUAT_W = 0
+        self.QUAT_X = 1
+        self.QUAT_Y = 2
+        self.QUAT_Z = 3
+        self.POS_X = 4
+        self.POS_Y = 5
+        self.POS_Z = 6
+        self.POS_LHP = 7
+        self.POS_LHR = 8
+        self.POS_LHY = 9
+        self.POS_LKP = 10
+        self.POS_LAP = 11
+        self.POS_LAR = 12
+        self.POS_RHP = 13
+        self.POS_RHR = 14
+        self.POS_RHY = 15
+        self.POS_RKP = 16
+        self.POS_RAP = 17
+        self.POS_RAR = 18
+
+        # generalized velocities
+        self.ANG_X = 0
+        self.ANG_Y = 1
+        self.ANG_Z = 2
+        self.VEL_X = 3
+        self.VEL_Y = 4
+        self.VEL_Z = 5
+        self.VEL_LHP = 6
+        self.VEL_LHR = 7
+        self.VEL_LHY = 8
+        self.VEL_LKP = 9
+        self.VEL_LAP = 10
+        self.VEL_LAR = 11
+        self.VEL_RHP = 12
+        self.VEL_RHR = 13
+        self.VEL_RHY = 14
+        self.VEL_RKP = 15
+        self.VEL_RAP = 16
+        self.VEL_RAR = 17
+
+        # joints
+        self.JOINT_LHP = 0
+        self.JOINT_LHR = 1
+        self.JOINT_LHY = 2
+        self.JOINT_LKP = 3
+        self.JOINT_LAP = 4
+        self.JOINT_LAR = 5
+        self.JOINT_RHP = 6
+        self.JOINT_RHR = 7
+        self.JOINT_RHY = 8
+        self.JOINT_RKP = 9
+        self.JOINT_RAP = 10
+        self.JOINT_RAR = 11
+
 #####################################################################################
 
 def standing_position():
@@ -129,6 +192,42 @@ def create_optimizer(model_file):
 
 #####################################################################################
 
+class G1_MPC(ModelPredictiveController):
+    """
+    A Model Predictive Controller for the Achilles humanoid.
+    """
+    def __init__(self, optimizer, q_guess, mpc_rate):
+        ModelPredictiveController.__init__(self, optimizer, q_guess, 19, 18, mpc_rate)
+
+    def UpdateNominalTrajectory(self, context):
+        """
+        Shift the reference trajectory based on the current position.
+        """
+        # instantiate the model instance indices
+        idx = IDX()
+
+        # Get the current state
+        x0 = self.state_input_port.Eval(context)
+        q0 = x0[:self.nq]
+        v0 = x0[self.nq:]
+
+        # Get the current nominal trajectory
+        prob = self.optimizer.prob()
+        q_nom = prob.q_nom
+        v_nom = prob.v_nom
+
+        # Shift the nominal trajectory
+        dt = self.optimizer.time_step()
+        vx = 0.25
+        for i in range(self.num_steps + 1):
+            q_nom[i][idx.POS_X] = q0[idx.POS_X] + vx * i * dt
+            v_nom[i][idx.VEL_X] = vx
+
+        self.optimizer.UpdateNominalTrajectory(q_nom, v_nom)
+
+
+#####################################################################################
+
 
 if __name__=="__main__":
 
@@ -170,12 +269,33 @@ if __name__=="__main__":
     # # Set up the trajectory optimization problem
     # # Note that the diagram and plant must stay in scope while the optimizer is
     # # being used
-    # optimizer, ctrl_diagram, ctrl_plant = create_optimizer(model_file)
-    # q_guess = [standing_position() for _ in range(optimizer.num_steps() + 1)]
+    optimizer, ctrl_diagram, ctrl_plant = create_optimizer(model_file)
+    q_guess = [standing_position() for _ in range(optimizer.num_steps() + 1)]
 
-    # # add the joystick
-    # joystick = builder.AddSystem(GamepadCommand(deadzone=0.05))
+    # Create the MPC controller and interpolator systems
+    mpc_rate = config['MPC']['mpc_rate']
+    controller = builder.AddSystem(G1_MPC(optimizer, q_guess, mpc_rate))
+
+    Bv = plant.MakeActuationMatrix()
+    N = plant.MakeVelocityToQDotMap(plant.CreateDefaultContext())
+    Bq = N@Bv
+    interpolator = builder.AddSystem(Interpolator(Bq.T, Bv.T))
     
+    # Wire the systems together
+    builder.Connect(
+        plant.get_state_output_port(), 
+        controller.GetInputPort("state"))
+    builder.Connect(
+        controller.GetOutputPort("optimal_trajectory"), 
+        interpolator.GetInputPort("trajectory"))
+    builder.Connect(
+        interpolator.GetOutputPort("control"), 
+        plant.get_actuation_input_port())
+    builder.Connect(
+        interpolator.GetOutputPort("state"), 
+        plant.get_desired_state_input_port(models[0])
+    )
+
     # Connect the plant to meshcat for visualization
     vis_config = VisualizationConfig()
     vis_config.publish_contacts = config['contact_vis']
@@ -189,9 +309,10 @@ if __name__=="__main__":
     # Set the initial state
     q0 = standing_position()
     v0 = np.array(config['v0'])
+
     plant.SetPositions(plant_context, q0)
     plant.SetVelocities(plant_context, v0)
-        
+
     # Simulate and play back on meshcat
     meshcat.StartRecording()
     st = time.time()
@@ -200,6 +321,6 @@ if __name__=="__main__":
     simulator.AdvanceTo(config['sim']['duration'])
     wall_time = time.time() - st
     print(f"sim time: {simulator.get_context().get_time():.4f}, "
-           f"wall time: {wall_time:.4f}")
+          f"wall time: {wall_time:.4f}")
     meshcat.StopRecording()
     meshcat.PublishRecording()
